36c76558376506d9ac246e2e25bac277d9bbe028
zzunko <zoran.zunko@gmail.com>
2013-04-02 10:17:48 +0200

    added faq extract

--- src/test/java/com/itagenten/test/ITCore0.java
+++ src/test/java/com/itagenten/test/ITCore0.java
@@ -9,26 +9,39 @@ import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.internal.csv.CSVParser;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ITCore0Test {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ITCore0 {
     private SolrServer server;
 
     @Before
     public void init() {
-        String url = "http://localhost:9090/solr/core0";
+        String url = "http://localhost:8080/solr/";
         server = new HttpSolrServer(url);
     }
 
     @Test
-    public void testIndexNotEmpty() throws SolrServerException {
+    public void testIndexNotEmpty() throws SolrServerException, IOException {
         System.out.println("dfkdposfpsdfjdpsg");
         SolrQuery query = new SolrQuery();
+        SolrDocumentList list = new SolrDocumentList();
+        CSVParser parser = new CSVParser(new FileReader("faq_extract.csv"));
+        String[][] response =  parser.getAllValues();
+        SolrInputDocument doc = new SolrInputDocument();
+
         query.setQuery("*:*");
         QueryResponse qr = server.query(query);
         SolrDocumentList sdl = qr.getResults();
+        System.out.println(sdl.getNumFound() + "-----------------------------------");
         assertNotNull("Results (SolrDocumentList) from Core 0 should not be null.", sdl);
-        assertTrue("Core 0 should not be empty.", sdl.getNumFound() > 0);
+
+        assertTrue("Core 0 should not be empty.", sdl.getNumFound() == 0);
     }
 }
